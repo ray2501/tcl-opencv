bd4e5e3d187da8f26bd2cf4b79d85dc96827a227
Danilo Chang <ray2501@gmail.com>
2021-09-21 20:00:29 +0800

    Bump version to v0.9

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded opencv 0.8 \
-	    [list load [file join $dir libtcl9opencv0.8.so] [string totitle opencv]]
+    package ifneeded opencv 0.9 \
+	    [list load [file join $dir libtcl9opencv0.9.so] [string totitle opencv]]
 } else {
-    package ifneeded opencv 0.8 \
-	    [list load [file join $dir libopencv0.8.so] [string totitle opencv]]
+    package ifneeded opencv 0.9 \
+	    [list load [file join $dir libopencv0.9.so] [string totitle opencv]]
 }
--- tcl-opencv.spec
+++ tcl-opencv.spec
@@ -5,7 +5,7 @@
 
 Name:          tcl-opencv
 Summary:       Tcl extension for OpenCV library
-Version:       0.8
+Version:       0.9
 Release:       0
 License:       MIT
 Group:         Development/Libraries/Tcl
